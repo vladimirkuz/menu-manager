88ecdf716083fba8ab7edc2125e361daeff2ba0f
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2020-01-07 20:47:39 -0500

    Added state for landing page

---
+++ src/landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Expresso from '../utils/Expresso';
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      employees: [],
+      menus: []
+    };
+  }
+}
