14bc3a7ead59d47fe344d00bba0779e6e2a7590b
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2020-01-09 23:09:56 -0500

    Added react components

--- src/landing.js
+++ src/landing.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './style.css';
 
-import Expresso from '../utils/Expresso';
+import Expresso from './util/Expresso';
 
-class Landing extends Component {
+import Menu from './menu';
+import Employee from './employee';
+
+class Landing extends React.Component {
   constructor(props) {
     super(props);
 
@@ -11,4 +15,64 @@ class Landing extends Component {
       menus: []
     };
   }
+
+  componentDidMount() {
+    Expresso.getMenus().then(menus => {
+      if (menus.length) {
+        const sortedMenus = this.sortItemNames(menus, 'title');
+        this.setState({menus: sortedMenus});
+      }
+    });
+
+    Expresso.getEmployees().then(employees => {
+      if (employees.length) {
+        const sortedEmployees = this.sortItemNames(employees, 'name');
+        this.setState({employees: sortedEmployees});
+      }
+    });
+  }
+
+  sortItemNames(items, field) {
+    return items.sort((item1, item2) => {
+      if (item2[field].toLowerCase() < item1[field].toLowerCase()) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+
+  renderMenus() {
+    return this.state.menus.map(menu => {
+      return <Menu className="item" key={menu.id} />;
+    });
+  }
+
+  renderEmployees() {
+    return this.state.employees.map(employee => {
+      return <Employee className="item" key={employee.id} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="Landing">
+        <h2>MANAGE MENUS</h2>
+
+        <div className="menu item-list">
+          {this.renderMenus()}
+        </div>
+
+        <div className="button">ADD</div>
+        <h2>MANAGE EMPLOYEES</h2>
+
+        <div className="employee item-list">
+          {this.renderEmployees()}
+        </div>
+        <div className="button">ADD</div>
+      </div>
+    );
+  }
 }
+
+export default Landing;
